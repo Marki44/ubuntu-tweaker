ff1547decece9200e7a1ccbae9aea318faf20309
Marki44 <ab4olute@gmail.com>
2025-12-13 16:30:04 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ BRANCH="main"
 MAIN_FILE="optimizer"
 
 RAW_BASE="https://raw.githubusercontent.com/${REPO_USER}/${REPO_NAME}/${BRANCH}"
-URL="${RAW_BASE}/${MAIN_FILE}"
+URL="${RAW_BASE}/${MAIN_FILE}?v=$(date +%s)"
 
 echo "[+] Downloading: $URL"
 
@@ -16,5 +16,4 @@ sudo apt-get install -y --no-install-recommends curl python3
 
 curl -fsSL "$URL" -o "/tmp/${MAIN_FILE}"
 chmod +x "/tmp/${MAIN_FILE}"
-
 sudo "/tmp/${MAIN_FILE}"
